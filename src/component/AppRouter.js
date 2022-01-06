26efeb9ead6dc4a6df951d7a3fbd7809f57ef6dc
Velgosh Nicolay <velgoshnicolay@MacBook-Pro-Velgosh.local>
2022-01-06 22:06:30 +0300

    UI

--- src/component/AppRouter.js
+++ src/component/AppRouter.js
@@ -1,12 +1,13 @@
 import React, { useContext } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import { AuthContext } from "../context";
-// import Posts from "../pages/Posts";
-// import About from "../pages/About";
-// import Error from "../pages/Error";
-// import PostIdPage from "../pages/PostIdPage";
+import Posts from "../pages/Posts";
+import About from "../pages/About";
+import Error from "../pages/Error";
+import PostIdPage from "../pages/PostIdPage";
 import { privateRoutes, publicRoutes } from "../router";
 import Loader from "./UI/loader/Loader";
+import Login from "../pages/Login";
 
 const AppRouter = () => {
   const { isLoading, isAuth, setIsAuth } = useContext(AuthContext);
@@ -14,24 +15,28 @@ const AppRouter = () => {
   console.log("isAuth", isAuth);
   console.log("isLoading", isLoading);
 
-  if (isLoading) {
-    console.log("Loader", isLoading);
-    return <Loader />;
-  }
+  // if (isLoading) {
+  //   console.log("Loader", isLoading);
+  //   return <Loader />;
+  // }
   console.log("Loaderof", isLoading);
+  console.log("isAuth", isAuth);
   return isAuth ? (
     <Routes>
-      {privateRoutes.map((route) => (
+      {/* {privateRoutes.map((route) => (
         <Route key={route.path} path={route.path} element={<route.element />} />
-      ))}
-      {/* <Route path="/" element={<Navigate to="/posts" />} /> */}
+      ))} */}
+
+      <Route path="/posts" element={<Posts />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/error" element={<Error />} />
+      <Route path="/posts/:id" element={<PostIdPage />} />
+      <Route path="/login" element={<Navigate to="/posts" />} />
       {/* <Route path="*" element={<Navigate to="/error" />} /> */}
     </Routes>
   ) : (
     <Routes>
-      {publicRoutes.map((route) => (
-        <Route key={route.path} path={route.path} element={<route.element />} />
-      ))}
+      <Route path="/login" element={<Login />} />
       <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
   );
--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import PostList from "../component/PostList";
 import PostForm from "../component/PostForm";
 import PostFilter from "../component/PostFilter";
@@ -11,6 +11,8 @@ import Loader from "../component/UI/loader/Loader";
 import { useFetching } from "../hooks/useFetching";
 import { getPageCount, getPagesArray } from "../utils/pages";
 import Pagination from "../component/UI/pagination/Pagination";
+import { useObserver } from "../hooks/useObserver";
+import MySelect from "../component/UI/select/MySelect";
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
@@ -21,8 +23,7 @@ const Posts = () => {
   const [page, setPage] = useState(1);
 
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
-  let pageArray = getPagesArray(totalPages);
+  const lastElement = useRef();
 
   const [fetchPosts, isPostLoading, postError] = useFetching(
     async (limit, page) => {
@@ -33,7 +34,11 @@ const Posts = () => {
     }
   );
 
-  useEffect(() => fetchPosts(limit, page), [page]);
+  useObserver(lastElement, page < totalPages, isPostLoading, () => {
+    setPage(page + 1);
+  });
+
+  useEffect(() => fetchPosts(limit, page), [page, limit]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -46,9 +51,7 @@ const Posts = () => {
 
   const changePage = (p) => {
     setPage(p);
-    fetchPosts(limit, page);
   };
-
   return (
     <div className="App">
       <MyButton
@@ -57,16 +60,41 @@ const Posts = () => {
       >
         Создать пост
       </MyButton>
+
       <MyButton style={{ marginTop: 30 }} onClick={fetchPosts}>
         fetch posts
       </MyButton>
+
       <MyModal visible={modal} setVisible={setModal}>
         Форма для создания поста
         <PostForm create={createPost} />
       </MyModal>
+
       <hr style={{ margin: "15px 0" }} />
+
       <PostFilter filter={filter} setFilter={setFilter} />
+
+      <MySelect
+        value={limit}
+        onChange={(value) => setLimit(value)}
+        defaultValue="Кол-во элементов на странице"
+        options={[
+          { value: 5, name: "5" },
+          { value: 10, name: "10" },
+          { value: 25, name: "25" },
+          { value: -1, name: "Показать все" },
+        ]}
+      />
+
       {postError && <h1>Произошла ошибка: {postError}</h1>}
+      <PostList
+        remove={removePost}
+        posts={sortedAndSearchedPosts}
+        title="Список постов"
+      />
+
+      <div ref={lastElement} style={{ height: 20, background: "red" }} />
+
       {isPostLoading && (
         <div
           style={{
@@ -78,11 +106,7 @@ const Posts = () => {
           <Loader />
         </div>
       )}
-      <PostList
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title="Список постов"
-      />
+
       <Pagination page={page} changePage={changePage} totalPages={totalPages} />
     </div>
   );
